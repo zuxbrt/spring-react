388ff65968f98ce690cecd996b1f6524a2e76768
zuxbrt <zmuhovic97@gmail.com>
2020-10-12 15:43:05 +0200

    home controller set

---
+++ src/main/java/com/example/springreact/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.springreact;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller // <1>
+public class HomeController {
+
+	@RequestMapping(value = "/") // <2>
+	public String index() {
+		return "index"; // <3>
+	}
+
+}
